7905b7b70cfce5648b1cb24e4f7800c3e812da0f
uxoa <palomita@gmail.com>
2024-11-04 13:46:13 +0100

    Feat: Updating exercice Paloma Kata

--- src/test/java/PalomaAccountTest.java
+++ src/test/java/PalomaAccountTest.java
@@ -9,14 +9,18 @@ public class PalomaAccountTest {
     @Test
     @DisplayName("test_TheFirstTest")
     void test_IfTheAccountHasAUniqueAccountNumberAndHasBalance(){
-        //GIVEN
+
+        //GIVEN:inicializo un nuevo objeto palomaAccount con dos atributos
         PalomaAccount palomaAccount = new PalomaAccount("12345", 1000.0);
 
-        //WHEN
+        //WHEN: Uso los métodos getAccountId() y getBalance() de PalomaAccount para obtener el número de cuenta y el saldo
         String accountId = palomaAccount.getAccountId();
         Double balance = palomaAccount.getBalance();
 
-        //THEN
+        //THEN: con assertEquals() verifico que los valores devueltos por getAccountId y getBalance son correctos
+        assertEquals("12345", accountId, "El número de cuenta es '12345'");
+        assertEquals(1000.0, balance, "El saldo es 1000.0");
+
 
 
     }
