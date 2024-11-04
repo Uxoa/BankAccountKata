8b11e1e954c4657148362c2b893683cdd18205d8
uxoa <palomita@gmail.com>
2024-11-04 13:31:16 +0100

    Feat: Initial commit Paloma Kata

--- src/main/java/org/example/PalomaAccount.java
+++ src/main/java/org/example/PalomaAccount.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class PalomaAccount {
+
+    private int accountId;
+    private Double balance;
+
+    public PalomaAccount(int accountId, Double balance) {
+        this.accountId = accountId;
+        this.balance = balance;
+    }
 }
--- src/test/java/PalomaAccountTest.java
+++ src/test/java/PalomaAccountTest.java
@@ -1,8 +1,23 @@
+import org.example.PalomaAccount;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PalomaAccountTest {
 
     @Test
+    @DisplayName("test_TheFirstTest")
+    void test_IfTheAccountHasAUniqueAccountNumberAndHasBalance(){
+        //GIVEN
+        PalomaAccount palomaAccount = new PalomaAccount();
+
+        //WHEN
+        String result =
+
+        //THEN
+        assertEquals(result);
+    }
 
 
 }
